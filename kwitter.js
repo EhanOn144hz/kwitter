080c47421269a0ee9d713cf7984fa41a614753aa
EhanOn144hz <88392390+EhanOn144hz@users.noreply.github.com>
2022-07-29 18:50:56 -0500

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,10 @@
+function adduser(){
+    username=document.getElementById("username").value;
+    localStorage.setItem("username",username);
+    window.location="kwitter_room.html";
+}
+function adduser(){
+    username=document.getElementById("password").value;
+    localStorage.setItem("password",password);
+    window.location="kwitter_room.html";
+}
